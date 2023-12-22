e01cb2a0165caa74d19b8d7dd4e1140e19af219e
Mohamed Hamdy <112737304+mohamdhamd@users.noreply.github.com>
2023-12-22 21:42:19 +0200

    Update man.js

--- man.js
+++ man.js
@@ -2,7 +2,7 @@ var typed = new Typed('.typing', {
     strings: [' '
     , 'AL mohtram Abdala' , 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala', 'Abdala Mousa','AL mohtram Abdala'],
     startDelay: 50,
-    typeSpeed: 150,
-    backSpeed: 100,
-    loopCount: Infinity,
+    typeSpeed:150,
+    BackSpeed:100,
+    loop:true
   });
